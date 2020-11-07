51d167abbda2d4e24c6e9af339fb234d03517694
Lakshya Kumar <kamrakumar275@gmail.com>
2020-11-07 11:26:36 +0530

    blog

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 console.log('Mybloger.com');
-let burger = document.getElementById('burger');
+/* let burger = document.getElementById('burger');
 let navbar =document.getElementById('navigation');
 let navlist =document.getElementById('navlist');
 let leftnav =document.getElementById('leftnav');
 let rightnav =document.getElementById('rightnav');
-
-
-
-
-
-
 console.log(burger);
 console.log(leftnav);
 console.log(rightnav);
 console.log(navbar);
-console.log(navlist);
+console.log(navlist); */
