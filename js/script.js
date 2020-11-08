8aa81dba3141cb029d49e9099bba6583fc833a2e
Lakshya Kumar <kamrakumar275@gmail.com>
2020-11-08 18:58:16 +0530

    upgrades

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 console.log('Mybloger.com');
+console.log('You Are Free To Use Console');
 let burger =document.getElementById('showhide');
 let navbar =document.getElementById('navigation');
 let navlist =document.getElementById('navlist');
 let leftnav =document.getElementById('leftnav');
 let rightnav =document.getElementById('rightnav');
 
-console.log(burger);
+// console.log(burger);
 // console.log(leftnav);
 // console.log(rightnav);
 // console.log(navbar);
