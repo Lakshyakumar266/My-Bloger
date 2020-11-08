30e55724c9a93446caeea8f9f08d17fcb3e4f8a5
Lakshya Kumar <kamrakumar275@gmail.com>
2020-11-08 18:47:45 +0530

    upgrades

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 console.log('Mybloger.com');
-/* let burger = document.getElementById('burger');
+let burger =document.getElementById('showhide');
 let navbar =document.getElementById('navigation');
 let navlist =document.getElementById('navlist');
 let leftnav =document.getElementById('leftnav');
 let rightnav =document.getElementById('rightnav');
+
 console.log(burger);
-console.log(leftnav);
-console.log(rightnav);
-console.log(navbar);
-console.log(navlist); */
+// console.log(leftnav);
+// console.log(rightnav);
+// console.log(navbar);
+// console.log(navlist);
+
+burger.addEventListener('click', ()=>{
+    navbar.classList.toggle('h-nav-resp')
+    navlist.classList.toggle('v-class-resp')
+    leftnav.classList.toggle('v-clas-resp')
+    rightnav.classList.toggle('v-inp-resp')
+})
